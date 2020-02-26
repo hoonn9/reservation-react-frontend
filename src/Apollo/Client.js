196664e065fb2241c592c3c3f56a842da7512469
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-02-26 16:47:57 +0900

    Deploy

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 const client = new ApolloClient({
-  uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://hxxns-reservation-react.herokuapp.com/",
   clientState: {
     defaults,
     resolvers
