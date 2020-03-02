318dbdf57517987a66d06e993572f889b3f8af06
koblmom <nayun9191@gmail.com>
2020-03-03 00:05:11 +0900

    테스트

--- src/Components/About/1/Presenter.js
+++ src/Components/About/1/Presenter.js
@@ -2,6 +2,5 @@ import React from "react";
 import "./main.css";
 
 export default () => {
-  console.log(process.env.PUBLIC_URL);
-  return <div>dasdadasds</div>;
+  return <div className="rdz">뭘해보삼?</div>;
 };
